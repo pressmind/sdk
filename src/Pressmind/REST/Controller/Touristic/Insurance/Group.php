994e330a1ac0d163c6e85ce19b7e0c49235c6ae4
Michael Graute <m.graute@team-braeutigam.de>
2021-01-04 14:07:47 +0100

    added REST-Controller for insurance groups

---
+++ src/Pressmind/REST/Controller/Touristic/Insurance/Group.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Pressmind\REST\Controller\Touristic\Insurance;
+
+
+use Pressmind\REST\Controller\AbstractController;
+
+class Group extends AbstractController
+{
+    protected $orm_class_name = '\\Pressmind\\ORM\\Object\\Touristic\\Insurance\\Group';
+}

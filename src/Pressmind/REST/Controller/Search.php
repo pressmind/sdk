a4ec8d4cee7c7cee6b6ff179148d3e594e66230c
michael-graute <m.graute@team-braeutigam.de>
2021-01-06 10:39:30 +0100

    reset validity condition in search REST controller

--- src/Pressmind/REST/Controller/Search.php
+++ src/Pressmind/REST/Controller/Search.php
@@ -25,22 +25,12 @@ class Search
         $return_filters_only = isset($searchparameters->return_filters_only) ? boolval($searchparameters->return_filters_only) : false;
         $conditions = [];
 
-        $has_validity_condition = false;
         foreach($search_conditions as $search_condition) {
-            if(strtolower($search_condition->type) == 'validity') {
-                $has_validity_condition = true;
-            }
             $condition_name = 'Pressmind\\Search\\Condition\\' . ucfirst($search_condition->type);
             /** @var ConditionInterface $condition */
             $condition = new $condition_name();
             $condition->setConfig($search_condition->config);
             $conditions[] = $condition;
-        }
-
-        if(!$has_validity_condition) {
-            $now = new \DateTime();
-            $validity_condition = Validity::create($now, $now);
-            $conditions[] = $validity_condition;
         }
 
         $sort = !is_null($search_sort) ? [$search_sort->property => $search_sort->direction] : null;

3b239623ac8bf7ed2c34d89b8320b950aa2ee8a1
michael-graute <m.graute@team-braeutigam.de>
2021-01-08 22:50:54 +0100

    deleting existing dataview before import

--- src/Pressmind/Import/DataView.php
+++ src/Pressmind/Import/DataView.php
@@ -4,6 +4,7 @@
 namespace Pressmind\Import;
 
 use Exception;
+use Pressmind\Log\Writer;
 use Pressmind\ORM\Object\DataView\SearchCondition;
 use Pressmind\REST\Client;
 
@@ -33,15 +34,23 @@ class DataView extends AbstractImport implements ImportInterface
         $client = new Client();
         try {
             $response = $client->sendRequest('Filter', 'search');
-            //print_r($response);
             $this->_checkApiResponse($response);
             if(isset($response->result) && is_array($response->result)) {
                 foreach ($response->result as $result) {
+                    $existing_dataviews = \Pressmind\ORM\Object\DataView::listAll(['id' => $result->id]);
+                    if(count($existing_dataviews) > 0) {
+                        $oldview = new \Pressmind\ORM\Object\DataView(intval($result->id));
+                        $oldview->delete(true);
+                    }
                     $data_view = new \Pressmind\ORM\Object\DataView();
                     $data_view->id = $result->id;
                     $data_view->name = $result->name;
                     $data_view->active = $result->active == 1;
                     $data_view->search_conditions = [];
+                    if(is_null($result->filter)) {
+                        $result->filter = [];
+                        $this->_log[] = Writer::write('DataView::import(): no filters are defined in ' . $result->name, Writer::OUTPUT_BOTH, 'import', Writer::TYPE_INFO);
+                    }
                     foreach ($result->filter as $filter_name => $filter_values) {
                         $search_condition = new SearchCondition();
                         $search_condition->class_name = $this->_class_map[$filter_name];
@@ -50,12 +59,6 @@ class DataView extends AbstractImport implements ImportInterface
                         $data_view->search_conditions[] = $search_condition;
                     }
                     $data_view->create();
-                    /*$brand->id = $result->id;
-                    $brand->name = $result->name;
-                    $brand->tags = $result->tags;
-                    $brand->description = $result->description;
-                    $brand->create();*/
-                    //print_r($result);
                 }
             }
         } catch (Exception $e) {

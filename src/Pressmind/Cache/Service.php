f9566281f4f817385f3bccdbf7554d71992abd6c
michael-graute <m.graute@team-braeutigam.de>
2021-01-19 10:41:01 +0100

    added call to cache cleanUp() function

--- src/Pressmind/Cache/Service.php
+++ src/Pressmind/Cache/Service.php
@@ -1,9 +1,17 @@
 <?php
 namespace Pressmind\Cache;
 
+use Exception;
+
 class Service {
     public function cleanUp()
     {
+        try {
+            $redis = new \Pressmind\Cache\Adapter\Redis();
+            $redis->cleanUp();
+        } catch (Exception $e) {
+            return 'ERROR: ' . $e->getMessage();
+        }
         return 'Cleanup has run';
     }
 }

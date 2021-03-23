0f9f185a50a05dcafb0fcb5bf708c3a75c186e60
michael-graute <m.graute@team-braeutigam.de>
2021-03-23 14:52:18 +0100

    changes to support new version of ib3

--- src/Pressmind/REST/Controller/Ibe.php
+++ src/Pressmind/REST/Controller/Ibe.php
@@ -8,14 +8,110 @@ use Pressmind\MVC\AbstractController;
 use Pressmind\ORM\Object\MediaObject;
 use Pressmind\ORM\Object\Touristic\Startingpoint\Option;
 
-class Ibe extends AbstractController
+class Ibe
 {
+    public $parameters;
+
+    public function pressmind_ib3_v2_test($params)
+    {
+        return ['success' => true, 'msg' => 'Test erfolgreich', 'debug' => $params];
+    }
+
+    public function pressmind_ib3_v2_get_touristic_object($params)
+    {
+        $this->parameters = $params['data'];
+        $settings = isset($this->parameters['settings']) ? $this->parameters['settings'] : null;
+        $booking = new Booking($this->parameters);
+        $mediaObject = new MediaObject($this->parameters['params']['imo']);
+        $result = [];
+
+        $result['date'] = $booking->getDate();
+        $result['transports'] = $booking->getTransports();
+        $insurances = $booking->getInsurances();
+        $housing_package = $booking->getHousingPackage()->toStdClass();
+        $housing_package->housing_options = [];
+
+        foreach ($housing_package->options as $option) {
+            $option->housing_package_name = $housing_package->name;
+            $housing_package->housing_options[] = $option;
+        }
+
+        $result['housing_packages'] = [$housing_package];
+        $result['insurances'] = $insurances;
+        $result['starting_points'] = $this->_getStartingPointOptionsForId($booking->getDate()->id_starting_point, 0, 10);
+        $result['exit_points'] = $this->_getExitPointOptionsForId($booking->getDate()->id_starting_point, 0, 10);
+        $result['extras'] = $booking->getAllAvailableExtras();
+        $result['has_pickup_services'] = $booking->hasPickServices();
+        $result['has_starting_points'] = $booking->hasStartingPoints();
+        $result['available_steps'] = [
+            'participants',
+            'finish'
+        ];
+
+        $result['available_detail_steps'] = [];
+
+        if(count($result['extras']) > 0) {
+            $result['available_detail_steps'][] = 'extras';
+            $result['available_steps'][] = 'extras';
+        }
+
+        if(count($result['insurances']) > 0) {
+            $result['available_detail_steps'][] = 'insurances';
+            $result['available_steps'][] = 'insurances';
+        }
+
+        if(isset($result['starting_points']['starting_point_options']) && count($result['starting_points']['starting_point_options']) > 0) {
+            $result['available_detail_steps'][] = 'starting_points';
+            $result['available_steps'][] = 'starting_points';
+        }
+        $image_info = null;
+        $images = $mediaObject->getValueByTagName('truetravel.teaser.image');
+        if(!is_null($images)) {
+            $image = $images[0];
+            $image_info['uri'] = substr($image->getUri('teaser'), 0, 4) == 'http' ? $image->getUri('teaser') : WEBSERVER_HTTP . $image->getUri('teaser');
+            $image_info['caption'] = $image->caption;
+            $image_info['alt'] = $image->alt;
+        }
+        $destination_name = null;
+        $destination_code = null;
+        if(isset($settings['general']['destination_tag_name']['value'])) {
+            $destinations = $mediaObject->getValueByTagName($settings['general']['destination_tag_name']['value']);
+            if (!is_null($destinations)) {
+                foreach ($destinations as $destination_array) {
+                    $destination = new \Pressmind\ORM\Object\CategoryTree\Item($destination_array->id_item);
+                    if (!empty($destination->code)) {
+                        $destination_name = $destination->name;
+                        $destination_code = $destination->code;
+                    }
+                }
+            }
+        }
+        $result['product'] = [
+            'title' => !empty($mediaObject->getValueByTagName('truetravel.headline')) ? strip_tags($mediaObject->getValueByTagName('truetravel.headline')) : $mediaObject->name,
+            'subtitle' => '',
+            'description' => '',
+            'name' => '',
+            'code' => $mediaObject->code,
+            'teaser_image' => $image_info,
+            'destination' => array('name' => $destination_name, 'code' => $destination_code),
+            'hotel_trust_text' => !empty($mediaObject->getValueByTagName('pressmind-ib3.hotel-trust-text')) ? $mediaObject->getValueByTagName('pressmind-ib3.hotel-trust-text') : null,
+            'trustbox_text' => !empty($mediaObject->getValueByTagName('pressmind-ib3.trustbox-text')) ? $mediaObject->getValueByTagName('pressmind-ib3.trustbox-text') : null,
+            'services_box_title' => !empty($mediaObject->getValueByTagName('pressmind-ib3.services-box-title')) ? $mediaObject->getValueByTagName('pressmind-ib3.services-box-title') : null,
+            'services_box_content' => !empty($mediaObject->getValueByTagName('pressmind-ib3.services-box-content')) ? $mediaObject->getValueByTagName('pressmind-ib3.services-box-content') : null,
+            'duration' => '',
+            'transport_type' => 'BUS',
+            'price_mix' => $booking->getBookingPackage()->price_mix
+        ];
+        return ['success' => true, 'data' => $result];
+    }
+
     /**
      * @return array
      * @throws Exception
      */
-    public function pressmind_ib3_get_touristic_object()
+    public function pressmind_ib3_get_touristic_object($params)
     {
+        $this->parameters = $params['data'];
         $booking = new Booking($this->parameters);
         $mediaObject = new MediaObject($this->parameters['params']['imo']);
         $settings = $this->parameters['settings'];

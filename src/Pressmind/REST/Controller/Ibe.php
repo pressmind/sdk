be03309eff536695bd5c2531ea0c784ca596673b
michael-graute <m.graute@team-braeutigam.de>
2021-04-27 14:56:08 +0200

    fixed some issues

--- src/Pressmind/REST/Controller/Ibe.php
+++ src/Pressmind/REST/Controller/Ibe.php
@@ -6,6 +6,7 @@ use Exception;
 use Pressmind\IBE\Booking;
 use Pressmind\MVC\AbstractController;
 use Pressmind\ORM\Object\MediaObject;
+use Pressmind\ORM\Object\Touristic\Housing\Package;
 use Pressmind\ORM\Object\Touristic\Startingpoint\Option;
 
 class Ibe
@@ -24,46 +25,6 @@ class Ibe
         $booking = new Booking($this->parameters);
         $mediaObject = new MediaObject($this->parameters['params']['imo']);
         $result = [];
-
-        $result['date'] = $booking->getDate();
-        $result['transports'] = $booking->getTransports();
-        $insurances = $booking->getInsurances();
-        $housing_package = $booking->getHousingPackage()->toStdClass();
-        $housing_package->housing_options = [];
-
-        foreach ($housing_package->options as $option) {
-            $option->housing_package_name = $housing_package->name;
-            $housing_package->housing_options[] = $option;
-        }
-
-        $result['housing_packages'] = [$housing_package];
-        $result['insurances'] = $insurances;
-        $result['starting_points'] = $this->_getStartingPointOptionsForId($booking->getDate()->id_starting_point, 0, 10);
-        $result['exit_points'] = $this->_getExitPointOptionsForId($booking->getDate()->id_starting_point, 0, 10);
-        $result['extras'] = $booking->getAllAvailableExtras();
-        $result['has_pickup_services'] = $booking->hasPickServices();
-        $result['has_starting_points'] = $booking->hasStartingPoints();
-        $result['available_steps'] = [
-            'participants',
-            'finish'
-        ];
-
-        $result['available_detail_steps'] = [];
-
-        if(count($result['extras']) > 0) {
-            $result['available_detail_steps'][] = 'extras';
-            $result['available_steps'][] = 'extras';
-        }
-
-        if(count($result['insurances']) > 0) {
-            $result['available_detail_steps'][] = 'insurances';
-            $result['available_steps'][] = 'insurances';
-        }
-
-        if(isset($result['starting_points']['starting_point_options']) && count($result['starting_points']['starting_point_options']) > 0) {
-            $result['available_detail_steps'][] = 'starting_points';
-            $result['available_steps'][] = 'starting_points';
-        }
         $image_info = null;
         $images = $mediaObject->getValueByTagName('truetravel.teaser.image');
         if(!is_null($images)) {
@@ -86,6 +47,7 @@ class Ibe
                 }
             }
         }
+        $result['date'] = $booking->getDate();
         $result['product'] = [
             'title' => !empty($mediaObject->getValueByTagName('truetravel.headline')) ? strip_tags($mediaObject->getValueByTagName('truetravel.headline')) : $mediaObject->name,
             'subtitle' => '',
@@ -100,8 +62,95 @@ class Ibe
             'services_box_content' => !empty($mediaObject->getValueByTagName('pressmind-ib3.services-box-content')) ? $mediaObject->getValueByTagName('pressmind-ib3.services-box-content') : null,
             'duration' => $booking->getBookingPackage()->duration,
             'transport_type' => 'BUS',
-            'price_mix' => $booking->getBookingPackage()->price_mix
+            'price_mix' => $booking->getBookingPackage()->price_mix,
         ];
+
+        $result['transports'] = $booking->getTransports();
+        $extras = $booking->getAllAvailableExtras();
+        $insurances = $booking->getInsurances();
+
+        $predefined_options = [];
+
+        if(isset($this->parameters['params']['iho'])) {
+            foreach ($this->parameters['params']['iho'] as $key => $value) {
+                $predefined_options[$key] = $value;
+            }
+        }
+
+        if(isset($this->parameters['params']['ido'])) {
+            $predefined_options[$this->parameters['params']['ido']] = 1;
+        }
+
+        if($booking->booking_package->price_mix == 'date_housing') {
+            if (isset($this->parameters['params']['idhp'])) {
+                $housing_packages = [$booking->getHousingPackage($this->parameters['params']['idhp'])->toStdClass()];
+            } else {
+                $housing_packages = $booking->booking_package->housing_packages;
+            }
+        } else if($booking->booking_package->price_mix == 'date_transport') {
+            $housing_package = new Package();
+            $housing_package->name = !empty($booking_package->name) ? $booking_package->name : $result['product']['title'];
+            /*if(isset($this->parameters['params']['ido'])) {
+                $option_id = $this->parameters['params']['ido'];
+            }*/
+            $option = [];
+            foreach ($result['transports'] as $transport) {
+                if(key_exists($transport->id, $predefined_options)) {
+                    $option = new \Pressmind\ORM\Object\Touristic\Option();
+                    $option->id = $transport->id;
+                    $option->name = null;
+                    $option->description_long = $transport->description_long;
+                    $option->code_ibe = $transport->code_ibe;
+                    $option->code = $transport->code;
+                    $option->max_pax = 10;
+                    $option->min_pax = 1;
+                    $option->occupancy_max = 10;
+                    $option->occupancy_min = 1;
+                    $option->occupancy = isset($predefined_options[$transport->id]) ? $predefined_options[$transport->id] : 1;;
+                    $option->price = $transport->price;
+                    $option->price_due = 'person_stay';
+                    $transport->price = 0;
+                }
+            }
+            $options = [$option];
+            $housing_package->options = $options;
+            $housing_packages[] = $housing_package;
+        } else {
+            $housing_package = new Package();
+            $housing_package->name = !empty($booking_package->name) ? $booking_package->name : $result['product']['title'];
+            $options = [];
+            foreach ($extras as $key => $extra) {
+                $extra->occupancy = isset($predefined_options[$extra->id]) ? $predefined_options[$extra->id] : 1;
+                $extra->occupancy_min = 1;
+                $extra->occupancy_max = $extra->max_pax > 0 ? $extra->max_pax : 10;
+                if ($extra->type == 'ticket' && $booking->booking_package->price_mix == 'date_ticket') {
+                    $options[] = $extra;
+                    unset($extras[$key]);
+                }
+                if ($extra->type == 'sightseeing' && $booking->booking_package->price_mix == 'date_sightseeing') {
+                    $options[] = $extra;
+                    unset($extras[$key]);
+                }
+                if ($extra->type == 'extra' && $booking->booking_package->price_mix == 'date_extra') {
+                    $options[] = $extra;
+                    unset($extras[$key]);
+                }
+            }
+            $housing_package->options = $options;
+            $housing_packages[] = $housing_package;
+        }
+
+        $result['debug'] = $booking;
+        $result['housing_packages'] = $housing_packages;
+        $result['insurances'] = $insurances;
+        $result['starting_points'] = $this->_getStartingPointOptionsForId($booking->getDate()->id_starting_point, 0, 10);
+        $result['exit_points'] = $this->_getExitPointOptionsForId($booking->getDate()->id_starting_point, 0, 10);
+        $result['extras'] = $extras;
+        $result['has_pickup_services'] = $booking->hasPickServices();
+        $result['has_starting_points'] = $booking->hasStartingPoints();
+        $result['id_ibe'] = $booking->getBookingPackage()->ibe_type;
+        $result['code_ibe'] = is_null($booking->getHousingPackage()) ? null : $booking->getHousingPackage()->code_ibe;
+
         return ['success' => true, 'data' => $result];
     }
 
@@ -190,7 +239,7 @@ class Ibe
         return array('total' => count($total_exit_point_options), 'exit_point_options' => $limited_exit_point_options);
     }
 
-    public function pressmind_ib3_get_exit_point() {
+    public function pressmind_ib3_v2_get_exit_point() {
         $id_starting_point = $this->getParameter('id_starting_point');
         $starting_point_option_code = $this->getParameter('starting_point_option_code');
         $exit_point = null;
@@ -202,16 +251,19 @@ class Ibe
         return ['exit_point' => $exit_point];
     }
 
-    public function pressmind_ib3_get_starting_point_options() {
-        $id_starting_point = $this->getParameter('id_starting_point');
-        $limit = $this->getParameter('limit') != null ? $this->getParameter('limit') : 10;
-        $start = $this->getParameter('start') != null ? $this->getParameter('start') : 0;
-        $zip = $this->getParameter('zip');
-        $radius = $this->getParameter('radius');
+    public function pressmind_ib3_v2_get_starting_point_options($params) {
+        $this->parameters = $params['data'];
+        $id_starting_point = $this->parameters['id_starting_point'];
+        $limit = $this->parameters['limit'] != null ? $this->parameters['limit'] : 10;
+        $start = isset($this->parameters['start']) ? $this->parameters['start'] : 0;
+        $zip = isset($this->parameters['zip']) ? $this->parameters['zip'] : null;
+        //return $zip;
+        //return $start;
+        $radius = isset($this->parameters['radius']) ? $this->parameters['radius'] : null;
         if(!is_null($zip)) {
-            return $this->_getZipRangeStartingPoints($id_starting_point, $zip, $radius, $start, $limit);
+            return ['success' => true, 'data' => $this->_getZipRangeStartingPoints($id_starting_point, $zip, $radius, $start, $limit)];
         } else {
-            return $this->_getStartingPointOptionsForId($id_starting_point, $start, $limit);
+            return ['success' => true, 'data' => $this->_getStartingPointOptionsForId($id_starting_point, $start, $limit)];
         }
     }
 

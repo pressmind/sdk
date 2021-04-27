adeb6c0b8d1eb10b8e90caed31b5ec2d473462bb
michael-graute <m.graute@team-braeutigam.de>
2021-04-27 14:56:52 +0200

    added function to check for php CLI

--- src/Pressmind/System/RequirementsCheck.php
+++ src/Pressmind/System/RequirementsCheck.php
@@ -6,5 +6,13 @@ namespace Pressmind\System;
 
 class RequirementsCheck
 {
+    public function checkPHPCliExecution()
+    {
+        $teststring = md5('php is fine!');
+        if(exec('php ' . __DIR__ . '/cli_execution.php ' . '"' . $teststring . '"') != $teststring) {
+            echo 'broken';
+        }
 
+        var_dump(exec('php ' . __DIR__ . '/cli_execution.php ' . '"' . $teststring . '"'));
+    }
 }

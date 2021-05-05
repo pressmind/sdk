9145bf3ac863aa961d5c28b2c606dc4bc3e44fe4
michael-graute <m.graute@team-braeutigam.de>
2021-05-05 10:21:36 +0200

    added support for 'backgroundMode'

--- src/Pressmind/REST/Controller/Import.php
+++ src/Pressmind/REST/Controller/Import.php
@@ -17,19 +17,35 @@ class Import
      */
     public function index($parameters)
     {
+        $config = Registry::getInstance()->get('config');
+
         if(!isset($parameters['id_media_object'])) {
             return ['Status' => 'Code 500: Parameter id_media_object is missing'];
+        }
+        if(isset($parameters['backgroundMode'])) {
+            $response = [
+                'status' => 200,
+                'msg' => 'BackgroundMode active: import run in background',
+                'url' => null,
+                'warnings' => null
+            ];
+            $import_script_path = APPLICATION_PATH . '/cli/import.php';
+            $php_binary = isset($config['server']['php_cli_binary']) && !empty($config['server']['php_cli_binary']) ? $config['server']['php_cli_binary'] : 'php';
+
+            $cmd = 'bash -c "exec nohup ' . $php_binary . ' '. $import_script_path .' mediaobject ' . $parameters['id_media_object'] . ' >/dev/null 2>&1 &"';
+
+            exec($cmd);
+
+            return $response;
         }
         $importer = new \Pressmind\Import('mediaobject');
         $preview_url = null;
         $importer->importMediaObject($parameters['id_media_object']);
-        $importer->postImport();
 
         $return = ['status' => 'Code 200: Import erfolgreich', 'url' => $preview_url, 'msg' => implode("\n", $importer->getLog())];
         if(isset($parameters['preview']) && $parameters['preview'] == 1) {
-            $media_object = new MediaObject($parameters['id_media_object']);
             $config = Registry::getInstance()->get('config');
-            $preview_url = str_replace(['{{id_media_object}}', '{{preview}}'], [$media_object->getId(), '1'], $config['data']['preview_url']);
+            $preview_url = str_replace(['{{id_media_object}}', '{{preview}}'], [$parameters['id_media_object'], '1'], $config['data']['preview_url']);
             if (substr($preview_url, 0, 4) != 'http') {
                 $preview_url = WEBSERVER_HTTP . $preview_url;
             }
@@ -38,3 +54,4 @@ class Import
         return $return;
     }
 }
+

404a19e3c8361a4d6091545b49149e29faa647ac
michael-graute <m.graute@team-braeutigam.de>
2021-07-15 16:10:26 +0200

    - added lowerCase to ORM filters

---
+++ src/Pressmind/ORM/Filter/Output/LowerCaseFilter.php
@@ -0,0 +1,20 @@
+<?php
+namespace Pressmind\ORM\Filter\Output;
+use Pressmind\ORM\Filter\FilterInterface;
+
+class LowerCaseFilter implements FilterInterface
+{
+    private $_errors = [];
+
+    public function filterValue($pValue)
+    {
+        if(empty($pValue) || $pValue === 'null') return null;
+        if(is_object($pValue)) return 'Error: Object to string conversion';
+        return strtolower($pValue);
+    }
+
+    public function getErrors()
+    {
+        return $this->_errors;
+    }
+}

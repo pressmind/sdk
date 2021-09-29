2a41c9450fb0f4a34e50965866490f4a4862b65d
Peter Bartl <71556602+peter-bartl@users.noreply.github.com>
2021-09-29 10:55:16 +0200

    improvement

--- src/Pressmind/Import.php
+++ src/Pressmind/Import.php
@@ -415,6 +415,7 @@ class Import
             $media_object->readRelations();
             $media_object->createSearchIndex();
             try {
+                $this->_log[] = Writer::write($this->_getElapsedTimeAndHeap() . ' Importer::importMediaObject(' . $id_media_object . '):  updating mongodb index', Writer::OUTPUT_BOTH, 'import', Writer::TYPE_INFO);
                 $media_object->createMongoDBIndex();
             }catch(Exception $e){
                 $this->_log[] = 'Error during creating MongoDBIndex: ' . $e->getMessage();

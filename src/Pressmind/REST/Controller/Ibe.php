2c152c23c46155a34130d94259c94ba3656fc478
Peter Bartl <71556602+peter-bartl@users.noreply.github.com>
2023-02-08 08:28:05 +0100

    improvement: reduce trickiness

--- src/Pressmind/REST/Controller/Ibe.php
+++ src/Pressmind/REST/Controller/Ibe.php
@@ -25,8 +25,8 @@ class Ibe
     public function pressmind_ib3_v2_get_touristic_object($params)
     {
         $this->parameters = $params['data'];
-        if(empty($this->parameters['params']['imo']) || empty($this->parameters['params']['idbp']) || empty($this->parameters['params']['idd'])){
-            return ['success' => false, 'msg' => 'error: parameters are missing imo, idbp, idd', 'data' => null];
+        if(empty($this->parameters['params']['imo']) || empty($this->parameters['params']['idbp']) || empty($this->parameters['params']['idd']) || empty($this->parameters['params']['iho'])){
+            return ['success' => false, 'msg' => 'error: parameters are missing imo, idbp, idd, iho', 'data' => null];
         }
 
         $settings = isset($this->parameters['settings']) ? $this->parameters['settings'] : null;
@@ -159,23 +159,18 @@ class Ibe
         }
 
         if($booking->getBookingPackage()->price_mix == 'date_housing') {
-                $housing_packages = [];
-                $housing_packages_list = $booking->getBookingPackage()->housing_packages;
-                foreach ($housing_packages_list as $housing_package) {
-                    if(!empty($this->parameters['params']['idhp']) && $housing_package->getId() != $this->parameters['params']['idhp']){
-                        continue;
-                    }
-                    $package = new Package($housing_package->id, true);
-                    $valid_options = [];
-                    foreach($package->options as $option){
-                        $current_saison = trim($option->season);
-                        if(in_array($current_saison, [$date->season, '-']) || empty($option->season)){
-                            $valid_options[] = new \Pressmind\ORM\Object\Touristic\Option($option->id, true);
-                        }
-                    }
-                    $package->options = $valid_options;
-                    $housing_packages[] = $package;
+            $Option = new \Pressmind\ORM\Object\Touristic\Option(array_key_first($this->parameters['params']['iho']));
+            $housing_packages = [];
+            $package = new Package($Option->id_housing_package, true);
+            $valid_options = [];
+            foreach($package->options as $option){
+                $current_saison = trim($option->season);
+                if(in_array($current_saison, [$date->season, '-']) || empty($option->season)){
+                    $valid_options[] = new \Pressmind\ORM\Object\Touristic\Option($option->id, true);
                 }
+            }
+            $package->options = $valid_options;
+            $housing_packages[] = $package;
         } else if($booking->booking_package->price_mix == 'date_transport') {
             $housing_package = new Package();
             $housing_package->name = !empty($booking_package->name) ? $booking_package->name : $result['product']['title'];

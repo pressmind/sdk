a13e7dae57a607e58afbe42b4970fa722792f4ee
Peter Bartl <71556602+peter-bartl@users.noreply.github.com>
2023-03-15 12:30:19 +0100

    api update

--- src/Pressmind/REST/Client.php
+++ src/Pressmind/REST/Client.php
@@ -18,7 +18,7 @@ class Client
     /**
      * @var string
      */
-    private $_api_endpoint = 'https://webcore.pressmind.net/v2-11/rest/';
+    private $_api_endpoint = 'https://webcore.pressmind.net/v2-12/rest/';
 
     /**
      * @var string

877eb0e0d6c410c42657d678940e38fe7803008c
Peter Bartl <71556602+peter-bartl@users.noreply.github.com>
2024-03-26 08:26:54 +0100

    speed improvement

--- src/Pressmind/Import/CategoryTree.php
+++ src/Pressmind/Import/CategoryTree.php
@@ -34,8 +34,26 @@ class CategoryTree extends AbstractImport implements ImportInterface
      */
     public function import()
     {
+        global $_RUNTIME_IMPORTED_CATEGORY_IDS;
+        if(!is_array($_RUNTIME_IMPORTED_CATEGORY_IDS)){
+            $_RUNTIME_IMPORTED_CATEGORY_IDS = [];
+        }
+        $ids = $this->_ids;
+        if(empty($this->_ids) && !is_array($this->_ids)){
+            $ids = [null];
+        }
+        $request = [];
+        foreach($ids as $category_id){
+            if(!in_array($category_id, $_RUNTIME_IMPORTED_CATEGORY_IDS)){
+                $request[] = $category_id;
+            }
+        }
+        if(empty($request)){
+           return true;
+        }
+        $_RUNTIME_IMPORTED_CATEGORY_IDS = array_merge($_RUNTIME_IMPORTED_CATEGORY_IDS, $request);
         $client = new Client();
-        $response = $client->sendRequest('Category', 'all', empty($this->_ids) ? [] : ['ids' => implode(',', $this->_ids)]);
+        $response = $client->sendRequest('Category', 'all', empty(array_filter($this->_ids)) ? [] : ['ids' => implode(',', $this->_ids)]);
         $this->_log[] = ' Importer::_importCategoryTrees(): REST request done';
         $this->_checkApiResponse($response);
         if (is_a($response, 'stdClass') && isset($response->result) && is_array($response->result)) {
@@ -46,7 +64,7 @@ class CategoryTree extends AbstractImport implements ImportInterface
                     $tree = new \Pressmind\ORM\Object\CategoryTree();
                     $tree->id = $tree_info->id;
                     $tree->name = $tree_info->name;
-                    try {
+                    try { // TODO optimize this.
                         $tree->delete();
                         $tree->create();
                     } catch (Exception $e) {

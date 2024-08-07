0bcc2544f35e980ac33a36c38dae5118ebc0edcf
Peter Bartl <71556602+peter-bartl@users.noreply.github.com>
2024-08-07 19:37:39 +0200

    fix missing props

--- src/Pressmind/ORM/Object/Touristic/Transport.php
+++ src/Pressmind/ORM/Object/Touristic/Transport.php
@@ -39,6 +39,8 @@ use Pressmind\System\I18n;
  * @property string $flight
  * @property boolean $dont_use_for_offers
  * @property boolean $use_earlybird
+ * @property string $time_departure
+ * @property string $time_arrival
  * @property Discount $discount
  * @property string $agencies
  */
@@ -439,6 +441,22 @@ class Transport extends AbstractObject
                 'title' => 'use_earlybird',
                 'name' => 'use_earlybird',
                 'type' => 'boolean',
+                'required' => false,
+                'validators' => NULL,
+                'filters' => NULL,
+            ],
+            'time_departure' => [
+                'title' => 'time_departure',
+                'name' => 'time_departure',
+                'type' => 'time',
+                'required' => false,
+                'validators' => NULL,
+                'filters' => NULL,
+            ],
+            'time_arrival' => [
+                'title' => 'time_departure',
+                'name' => 'time_departure',
+                'type' => 'time',
                 'required' => false,
                 'validators' => NULL,
                 'filters' => NULL,

811f115c9fe1b663a8873818c1f957b991ac0ae6
Peter Bartl <71556602+peter-bartl@users.noreply.github.com>
2024-09-11 15:05:23 +0200

    fix

--- src/Pressmind/Import/Port.php
+++ src/Pressmind/Import/Port.php
@@ -19,15 +19,19 @@ class Port extends AbstractImport implements ImportInterface
         try {
             $response = $client->sendRequest('Ports', 'getAll');
             $this->_checkApiResponse($response);
-            if(isset($response->result) && is_array($response->result)) {
+            if (isset($response->result) && is_array($response->result)) {
                 foreach ($response->result as $result) {
-                    $port = new \Pressmind\ORM\Object\Port();
-                    $port->id = $result->id;
-                    $port->code = $result->code;
-                    $port->active = $result->active;
-                    $port->name = $result->name;
-                    $port->description = $result->description;
-                    $port->create();
+                    try {
+                        $port = new \Pressmind\ORM\Object\Port();
+                        $port->id = $result->id;
+                        $port->code = $result->code;
+                        $port->active = $result->active;
+                        $port->name = $result->name;
+                        $port->description = $result->description;
+                        $port->create();
+                    } catch (Exception $e) {
+                        $this->_errors[] = $e->getMessage();
+                    }
                 }
             }
         } catch (Exception $e) {
--- src/Pressmind/ORM/Object/Port.php
+++ src/Pressmind/ORM/Object/Port.php
@@ -14,6 +14,7 @@ class Port extends AbstractObject
 {
 
     protected $_dont_use_autoincrement_on_primary_key = true;
+    protected $_replace_into_on_create = true;
 
     protected $_definitions = [
         'class' => [

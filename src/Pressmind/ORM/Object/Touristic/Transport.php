d0f5f16ae28431f245debdbd118cef8dc1aa99a0
Peter Bartl <71556602+peter-bartl@users.noreply.github.com>
2024-09-18 17:07:18 +0200

    rollback

--- src/Pressmind/ORM/Object/Touristic/Transport.php
+++ src/Pressmind/ORM/Object/Touristic/Transport.php
@@ -48,8 +48,6 @@ class Transport extends AbstractObject
 {
     protected $_dont_use_autoincrement_on_primary_key = true;
 
-    protected $_replace_into_on_create = true;
-
     protected $_definitions = array(
         'class' => [
             'name' => self::class,

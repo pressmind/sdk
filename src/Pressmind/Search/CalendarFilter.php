f5303be2a983fb750aa0448498353c25df5170cc
Peter Bartl <71556602+peter-bartl@users.noreply.github.com>
2025-03-20 10:47:20 +0100

    improvement

--- src/Pressmind/Search/CalendarFilter.php
+++ src/Pressmind/Search/CalendarFilter.php
@@ -14,4 +14,32 @@ class CalendarFilter
     public $airport = null;
     public $startingpoint_id_city = null;
     public $agency = null;
+
+    /**
+     * @return bool
+     */
+    public function initFromGet()
+    {
+        $something_set = false;
+        $params = [
+            'id',
+            'id_booking_package',
+            'id_housing_package',
+            'housing_package_code_ibe',
+            'occupancy',
+            'transport_type',
+            'duration',
+            'airport',
+            'startingpoint_id_city',
+            'agency'
+        ];
+        foreach ($params as $param) {
+            if (isset($_GET[$param])) {
+                $something_set = true;
+                $this->$param = preg_replace('/[^a-zA-Z0-9_\-,]/', '', $_GET[$param]);
+            }
+        }
+        return $something_set;
+    }
+
 }
--- src/Pressmind/Search/CheapestPrice.php
+++ src/Pressmind/Search/CheapestPrice.php
@@ -36,4 +36,68 @@ class CheapestPrice
      * @var int[] $state_fallback_order
      */
     public $state_fallback_order = [self::STATE_BOOKABLE, self::STATE_REQUEST, self::STATE_STOP];
+
+    /**
+     * @return void
+     */
+    public function initFromGet()
+    {
+        $params = [
+            'id',
+            'duration_from',
+            'duration_to',
+            'occupancies',
+            'occupancies_disable_fallback',
+            'date_from',
+            'date_to',
+            'price_from',
+            'price_to',
+            'id_date',
+            'id_option',
+            'id_booking_package',
+            'id_housing_package',
+            'housing_package_code_ibe',
+            'transport_types',
+            'transport_1_airport',
+            'origin',
+            'agency',
+            'state',
+            'id_startingpoint_option',
+            'startingpoint_option_name',
+            'startingpoint_id_city'
+        ];
+        foreach ($params as $param) {
+            if (property_exists($this, $param) && !empty($_GET[$param])) {
+                if($param == 'occupancies' || $param == 'transport_types' || $param == 'transport_1_airport'){
+                    $this->$param = explode(',', $_GET[$param]);
+                    continue;
+                }
+                if($param == 'occupancies_disable_fallback'){
+                    $this->$param = (bool)$_GET[$param];
+                    continue;
+                }
+                if($param == 'state'){
+                    $this->$param = (int)$_GET[$param];
+                    if($this->$param === 0){
+                        $this->$param = null;
+                    }
+                    continue;
+                }
+                if($param == 'price_from' || $param == 'price_to'){
+                    $this->$param = (float)$_GET[$param];
+                    continue;
+                }
+                if($param == 'duration_from' || $param == 'duration_to'){
+                    $this->$param = (int)$_GET[$param];
+                    continue;
+                }
+                if($param == 'date_from' || $param == 'date_to'){
+                    $this->$param = \DateTime::createFromFormat('Ymd', $_GET[$param]);
+                    continue;
+                }
+                $this->$param = preg_replace('/[^a-zA-Z0-9_\-,]/', '', $_GET[$param]);
+            }
+        }
+    }
+
 }

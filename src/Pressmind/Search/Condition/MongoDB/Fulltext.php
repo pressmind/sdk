46853b8648e219d8913b487985c42f369c2b4b49
Peter Bartl <71556602+peter-bartl@users.noreply.github.com>
2025-06-11 08:55:18 +0200

    improvement

--- src/Pressmind/Search/Condition/MongoDB/Fulltext.php
+++ src/Pressmind/Search/Condition/MongoDB/Fulltext.php
@@ -29,32 +29,30 @@ class Fulltext
     {
         if ($type == 'first_match') {
             return [
-                '$text' => [
-                    '$search' => '"' . $this->_searchString . '"'
+                'fulltext' => [
+                        '$regex' => '\\b'.$this->_searchString,
+                        '$options' => 'i'
                 ]
             ];
         } elseif ($type == 'project') {
             return ['$project' => [
-                'best_price_meta' => 1,
-                'categories' => 1,
-                'code' => 1,
-                'dates_per_month' => 1,
-                'departure_date_count' => 1,
-                'description' => 1,
-                'groups' => 1,
-                'id_media_object' => 1,
-                'id_object_type' => 1,
-                'last_modified_date' => 1,
-                'possible_durations' => 1,
-                'prices' => 1,
-                'url' => 1,
-                'valid_from' => 1,
-                'valid_to' => 1,
-                'visibility' => 1,
-                'score' => [
-                    '$meta' => 'textScore'
+                    'best_price_meta' => 1,
+                    'categories' => 1,
+                    'code' => 1,
+                    'dates_per_month' => 1,
+                    'departure_date_count' => 1,
+                    'description' => 1,
+                    'groups' => 1,
+                    'id_media_object' => 1,
+                    'id_object_type' => 1,
+                    'last_modified_date' => 1,
+                    'possible_durations' => 1,
+                    'prices' => 1,
+                    'url' => 1,
+                    'valid_from' => 1,
+                    'valid_to' => 1,
+                    'visibility' => 1
                 ]
-            ]
             ];
         }
         return null;

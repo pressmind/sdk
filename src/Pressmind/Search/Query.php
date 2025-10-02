533a2c043d5f019b58151fb6d4299ff085ec3beb
Peter Bartl <71556602+peter-bartl@users.noreply.github.com>
2025-10-02 11:20:08 +0200

    improvement

--- src/Pressmind/Search/Query.php
+++ src/Pressmind/Search/Query.php
@@ -148,6 +148,7 @@ class Query
                     $item['cheapest_price']->startingpoint_id_city = !empty($document['prices']['startingpoint_option']['id']) ? $document['prices']['startingpoint_option']['id'] : null;
                     $item['cheapest_price']->startingpoint_city = !empty($document['prices']['startingpoint_option']['city']) ? $document['prices']['startingpoint_option']['city'] : null;
                     $item['cheapest_price']->startingpoint_zip = !empty($document['prices']['startingpoint_option']['zip']) ? $document['prices']['startingpoint_option']['zip'] : null;
+                    $item['cheapest_price']->quota_pax = is_int($document['prices']['quota_pax']) ? $document['prices']['quota_pax'] : 999;
                 } else {
                     $item['cheapest_price'] = null;
                     $document['prices'] = null;
